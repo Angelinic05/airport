9722d001fea83199c566f7ba8ecc67aeca76ad8b
MichelleRiveros <michelle.riveros2303@hotmail.com>
2024-06-19 20:23:29 -0500

    feat: :sparkles: Se agregan los atributos a cada entidad
    Se agregan los atributos a sus respectivas entidades, se generan importaciones de "Date" con formato de java para la implementacion del dato

--- src/main/java/com/campuslands/moduls/model/domain/Model.java
+++ src/main/java/com/campuslands/moduls/model/domain/Model.java
@@ -1,5 +1,33 @@
 package com.campuslands.moduls.model.domain;
 
 public class Model {
-    
+    int id;
+    String name;
+    int idManufactures;
+
+    public Model (){}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getIdManufactures() {
+        return idManufactures;
+    }
+
+    public void setIdManufactures(int idManufactures) {
+        this.idManufactures = idManufactures;
+    }
 }
--- src/main/java/com/campuslands/moduls/revemployee/domain/Revemployee.java
+++ src/main/java/com/campuslands/moduls/revemployee/domain/Revemployee.java
@@ -1,5 +1,36 @@
 package com.campuslands.moduls.revemployee.domain;
 
 public class Revemployee {
+    int id;
+    int idEmployee;
+    int idRevision;
+
+    public Revemployee (){}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIdEmployee() {
+        return idEmployee;
+    }
+
+    public void setIdEmployee(int idEmployee) {
+        this.idEmployee = idEmployee;
+    }
+
+    public int getIdRevision() {
+        return idRevision;
+    }
+
+    public void setIdRevision(int idRevision) {
+        this.idRevision = idRevision;
+    }
+
+    
     
 }
