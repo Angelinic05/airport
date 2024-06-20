2d1ba013c821c3f8cad7d79c0e96c5bc480528c0
N3croide <106851453+N3croide@users.noreply.github.com>
2024-06-19 22:28:30 -0500

    feat: :sparkles: Agg Atributos
    Se agrega atributos a las entidades de airline, airport, ariportAirline, city, customer, country y documenttype

--- src/main/java/com/campuslands/moduls/airportAirline/domain/AirportAirline.java
+++ src/main/java/com/campuslands/moduls/airportAirline/domain/AirportAirline.java
@@ -2,4 +2,43 @@ package com.campuslands.moduls.airportAirline.domain;
 
 public class AirportAirline {
     
+    protected int id;
+    protected int idAirline;
+    protected int idAirport;
+    
+    public AirportAirline() {
+    }
+
+    public AirportAirline(int id, int idAirline, int idAirport) {
+        this.id = id;
+        this.idAirline = idAirline;
+        this.idAirport = idAirport;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIdAirline() {
+        return idAirline;
+    }
+
+    public void setIdAirline(int idAirline) {
+        this.idAirline = idAirline;
+    }
+
+    public int getIdAirport() {
+        return idAirport;
+    }
+
+    public void setIdAirport(int idAirport) {
+        this.idAirport = idAirport;
+    }
+
+    
+
 }
--- src/main/java/com/campuslands/moduls/documenttype/domain/Documenttype.java
+++ src/main/java/com/campuslands/moduls/documenttype/domain/Documenttype.java
@@ -2,4 +2,33 @@ package com.campuslands.moduls.documenttype.domain;
 
 public class Documenttype {
     
+    protected int id;
+    protected String name;
+    
+    public Documenttype() {
+    }
+
+    public Documenttype(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    
+
 }
