4a2d347778ecc7b2df45d1dd9c2c2276de5e4a8f
MichelleRiveros <michelle.riveros2303@hotmail.com>
2024-06-22 13:42:01 -0500

    feat: :fire: Se remueve importacion innecesaria
    Se remueve importacion innecesaria en revemployee

--- src/main/java/com/campuslands/modules/revemployee/adapter/out/RevemployeeMySQLRepository.java
+++ src/main/java/com/campuslands/modules/revemployee/adapter/out/RevemployeeMySQLRepository.java
@@ -8,7 +8,6 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import com.campuslands.modules.plane.domain.Plane;
 import com.campuslands.modules.revemployee.domain.Revemployee;
 import com.campuslands.modules.revemployee.infrastructure.RevemployeeRepository;
 
