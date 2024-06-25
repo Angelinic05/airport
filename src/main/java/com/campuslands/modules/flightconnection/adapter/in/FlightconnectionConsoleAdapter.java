8382debfe1a6d76e464875ce1f2228a9eab44a3d
N3croide <106851453+N3croide@users.noreply.github.com>
2024-06-25 18:11:26 -0500

    feat: :zap: asignar aeronave

--- src/main/java/com/campuslands/modules/flightconnection/adapter/in/FlightconnectionConsoleAdapter.java
+++ src/main/java/com/campuslands/modules/flightconnection/adapter/in/FlightconnectionConsoleAdapter.java
@@ -27,10 +27,13 @@ public class FlightconnectionConsoleAdapter {
                 case 1:
                     System.out.print("Ingrese el numero de connexión: ");
                     String createConnectionNumber = scanner.nextLine();
-                    System.out.println("Lista de trayectos disponibles: ");
-                    int createIdTrip = flightconnectionService.avaliableFlightsForTrip();
+                    System.out.println("Ingrese el numero del Viaje: ");
+                    int createIdTrip = scanner.nextInt();
+                    scanner.nextLine();
+
                     System.out.println("Lista de aviones disponibles: ");
                     int createIdPlane = flightconnectionService.avaliablePlanesForTrip();
+                    createIdPlane = createIdPlane == -1 ? null : createIdPlane;
 
                     System.out.print("Ingrese el ID del aeropuerto: ");
                     int createIdAirport = scanner.nextInt();
@@ -100,6 +103,15 @@ public class FlightconnectionConsoleAdapter {
                         System.out.println("ID:" + p.getId() + ", numero de connexión : " + p.getConnectionNumber() + ", idTrip: " + p.getIdTrip() + ", idPlane: " + p.getIdPlane() + ", idAirport: " + p.getIdAirport());
                     });
                     break;
+                case 6:
+                    System.out.println("Lista de Trayectos sin aeronave: ");
+                    int idFlightconnection = flightconnectionService.avaliableFlightsForTrip();
+                    Flightconnection flightconnection2 = flightconnectionService.findByIdFlightconnection(idFlightconnection).get();
+                    int idPlane = flightconnectionService.avaliablePlanesForTrip();
+                    flightconnection2.setIdPlane(idPlane);
+                    flightconnectionService.updateFlightconnection(flightconnection2);
+                    
+                    break;
 
                 case 0:
                     System.out.println("Saliendo...");
@@ -117,19 +129,20 @@ public class FlightconnectionConsoleAdapter {
         System.out.println("3. Buscar una conexión de vuelo por ID");
         System.out.println("4. Eliminar una conexión de vuelo");
         System.out.println("5. Listar todas las conexiónes de vuelo");
+        System.out.println("6. Asignar aeronave conexiónes de vuelo");
         System.out.println("0. Salir");
         System.out.println("");
         System.out.print("Ingrese la opcion: ");
         int choice = -1;
 
-        while (choice < 0 || choice > 5) {
+        while (choice < 0 || choice > 6) {
             try {
                 choice = Integer.parseInt(scanner.nextLine());
                 if (choice > 6) {                    
-                    System.out.println("Ingrese una opcion valida (1 - 5).");
+                    System.out.println("Ingrese una opcion valida (1 - 6).");
                 }
             } catch (Exception e) {
-                System.out.println("Ingrese una opcion valida (1 - 5).");
+                System.out.println("Ingrese una opcion valida (1 - 6).");
             }
         }
         return choice;
